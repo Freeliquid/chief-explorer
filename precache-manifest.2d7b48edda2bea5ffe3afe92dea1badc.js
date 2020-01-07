5d358e5e510cfab98fba48a335650c8ff6799456
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-01-07 13:00:20 -0300

    Updates

---
+++ precache-manifest.2d7b48edda2bea5ffe3afe92dea1badc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a909b03634592474d6666dbec2bf2519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d45998ff863bb4fdee66",
+    "url": "/static/css/main.3802e450.chunk.css"
+  },
+  {
+    "revision": "16cc0eb5ea1fb870f179",
+    "url": "/static/js/2.ffe5d460.chunk.js"
+  },
+  {
+    "revision": "8d0c249183bb2933444346a44c127f0f",
+    "url": "/static/js/2.ffe5d460.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d45998ff863bb4fdee66",
+    "url": "/static/js/main.81788148.chunk.js"
+  },
+  {
+    "revision": "6bdc8d94eb86d67afbfc",
+    "url": "/static/js/runtime-main.99faa813.js"
+  },
+  {
+    "revision": "2854f3a0b86bd2c41e0ffae651c43a6c",
+    "url": "/static/media/makerdao.2854f3a0.svg"
+  }
+]);
